94b53c67bff7d4bd0acde51e61239131643d6bf3
MHS-20 <muhamad.salman2001@gmail.com>
2025-05-24 12:16:27 +0200

    (lab7): completed exercise 1

--- pps-lab07-ScalaMechanisms/src/main/scala/ex1/Parser.scala
+++ pps-lab07-ScalaMechanisms/src/main/scala/ex1/Parser.scala
@@ -14,7 +14,9 @@ abstract class Parser[T]:
     (seq forall parse) & end // note &, not &&
 
 object Parsers:
-  val todo = ??? // put the extensions here..
+  extension (s: String)
+    def charParser(): Parser[Char] = new BasicParser(s.toSet)
+
 class BasicParser(chars: Set[Char]) extends Parser[Char]:
   override def parse(t: Char): Boolean = chars.contains(t)
   override def end: Boolean = true
@@ -23,7 +25,7 @@ trait NonEmpty[T] extends Parser[T]:
   private[this] var empty = true
   abstract override def parse(t: T): Boolean =
     empty = false;
-    super.parse(t) // who is super??
+    super.parse(t)
   abstract override def end: Boolean = !empty && super.end
 
 class NonEmptyParser(chars: Set[Char])
@@ -31,11 +33,34 @@ class NonEmptyParser(chars: Set[Char])
     with NonEmpty[Char]
 
 trait NotTwoConsecutive[T] extends Parser[T]:
-  val todo = ???
-// ???
+  private[this] var previous: Option[T] = None
+  private[this] var equal = false
+  abstract override def parse(t: T): Boolean =
+    if previous contains t
+    then equal = true
+    else previous = Some(t)
+    super.parse(t)
+  abstract override def end: Boolean = !equal && super.end
 
 class NotTwoConsecutiveParser(chars: Set[Char])
-    extends BasicParser(chars) // with ????
+    extends BasicParser(chars)
+    with NotTwoConsecutive[Char]
+
+class NotEmptyTwoConsecutiveParser(chars: Set[Char])
+    extends BasicParser(chars)
+    with NotTwoConsecutive[Char]
+    with NonEmpty[Char]
+
+trait ShorterThanN[T](n:Int) extends Parser[T]:
+  private[this] var length: Int = 0
+  abstract override def parse(t: T): Boolean =
+    length += 1
+    super.parse(t)
+  abstract override def end: Boolean = length < n && super.end
+
+class ShorterThanNParser(chars: Set[Char], n: Int)
+    extends BasicParser(chars)
+    with ShorterThanN[Char](n)
 
 @main def checkParsers(): Unit =
   def parser = new BasicParser(Set('a', 'b', 'c'))
@@ -64,7 +89,14 @@ class NotTwoConsecutiveParser(chars: Set[Char])
   println(parserNTCNE.parseAll("XYYZ".toList)) // false
   println(parserNTCNE.parseAll("".toList)) // false
 
-  def sparser: Parser[Char] = ??? // "abc".charParser()
+  def parserSTN = new ShorterThanNParser(Set('a', 'b', 'c'), 3)
+    println(parserSTN.parseAll("ab".toList)) // true
+    println(parserSTN.parseAll("abc".toList)) // false
+    println(parserSTN.parseAll("abcd".toList)) // false
+    println(parserSTN.parseAll("".toList)) // true
+
+  import ex1.Parsers.charParser
+  def sparser: Parser[Char] = "abc".charParser()
   println(sparser.parseAll("aabc".toList)) // true
   println(sparser.parseAll("aabcdc".toList)) // false
   println(sparser.parseAll("".toList)) // true
